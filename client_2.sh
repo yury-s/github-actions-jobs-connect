38cdd40f4c6b83f57c83fb17a9b47eaa84dcda74
Yury Semikhatsky <yurys@chromium.org>
2025-03-30 11:16:19 -0700

    add --connect-timeout 2

--- client_2.sh
+++ client_2.sh
@@ -5,7 +5,7 @@ wg-quick up server_config/wg0.conf
 
 echo "Downloading test page from the server..."
 # Wait for server to be ready
-until curl -sSf http://192.168.166.1:8080 >/dev/null; do
+until curl -sSf --connect-timeout 2 http://192.168.166.1:8080 >/dev/null; do
   sleep 0.2
 done
 curl http://192.168.166.1:8080/test.html
